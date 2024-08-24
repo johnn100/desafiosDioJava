1c94f6e87e6f58de94e2a8f919a534ee4ffc195a
Jhonattan <jho.pasquali@hotmail.com>
2024-08-24 08:59:01 -0300

    Atualização Desafio

---
+++ POO/src/Main.java
@@ -0,0 +1,65 @@
+import java.time.LocalDate;
+
+import br.com.dio.desafio.Bootcamp;
+import br.com.dio.desafio.Curso;
+import br.com.dio.desafio.Dev;
+import br.com.dio.desafio.Mentoria;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição do curso de java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso Python");
+        curso2.setDescricao("Descrição do curso de python");
+        curso2.setCargaHoraria(16);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de Java");
+        mentoria.setDescricao("Descerição Mentoria Java");
+        mentoria.setData(LocalDate.now());
+
+        //System.out.println(curso1);
+        //System.out.println(curso2);
+        //System.out.println(mentoria);
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devJhonattan = new Dev();
+        devJhonattan.setNome("Jhonattan");
+        devJhonattan.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Jhonattan: " + devJhonattan.getConteudosInscritos());
+        devJhonattan.progredir();
+        System.out.println("----");
+        System.out.println("Conteúdos Inscritos Jhonattan: " + devJhonattan.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Jhonattan: " + devJhonattan.getConteudosConcluidos());
+        System.out.println("XP: " + devJhonattan.calcularTotalXp());
+
+        System.out.println("--------------------------------");
+
+        Dev devJoao = new Dev();
+        devJoao.setNome("João");
+        devJoao.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos João: " + devJoao.getConteudosInscritos());
+        devJoao.progredir();
+        devJoao.progredir();
+        devJoao.progredir();
+        System.out.println("----");
+        System.out.println("Conteúdos Inscritos João: " + devJoao.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos João: " + devJoao.getConteudosConcluidos());
+        System.out.println("XP: " + devJoao.calcularTotalXp());
+
+
+    }
+}
+
+
+//Parei no concluindo a criação das classes do domínio.
